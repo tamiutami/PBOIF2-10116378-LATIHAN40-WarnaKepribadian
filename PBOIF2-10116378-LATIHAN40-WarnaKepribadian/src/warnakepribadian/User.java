7f1330f896b61bb68a8fb0bfaaf4928fb567f345
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-12 01:36:28 +0700

    Add files via upload

---
+++ PBOIF2-10116378-LATIHAN40-WarnaKepribadian/src/warnakepribadian/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warnakepribadian;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class User {
+ private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+}
